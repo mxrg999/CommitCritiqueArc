a271bbe6086c44bf5345ce867312007cbff17f64
mxrg999 <mxrg999@gmail.com>
2023-11-10 23:03:35 +0100

    initial webhook structure

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 from flask import Flask, request, abort
 from webhook_handler import handle_push_event
 import os
+import hmac
+import hashlib
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    # Retrieve the secret from the environment
     github_secret = os.environ.get('GITHUB_SECRET').encode()
 
-    # Validate payload from GitHub
-    # ... validation logic here ...
+    # Get the signature from the request header
+    signature_header = request.headers.get('X-Hub-Signature')
+
+    # Compute the signature
+    signature = hmac.new(github_secret, request.data, hashlib.sha1).hexdigest()
+
+    # Validate the signature
+    if not hmac.compare_digest(signature_header, 'sha1=' + signature):
+        abort(403)  # Abort if the signature is not valid
+    
+    
+    github_api_token = os.getenv('GITHUB_API_TOKEN')
+
 
     # Handle the event
     event = request.headers.get('X-GitHub-Event', 'ping')
     if event == "push":
-        handle_push_event(request.json)
+        handle_push_event(request.json, github_api_token)
         return '', 204
     else:
         # For now, only push events
--- webhook_handler.py
+++ webhook_handler.py
@@ -1,9 +1,14 @@
-def handle_push_event(payload):
-    # Extract commit information from the payload
-    for commit in payload.get('commits', []):
-        comment_on_commit(commit['url'], commit['id'])
+import requests
 
-def comment_on_commit(commit_url, commit_id):
-    # Will use GitHub API to post a comment on the commit
-    # GitHub token and the API will be used here
-    pass
+def handle_push_event(payload, github_api_token):
+    # Iterate through commits and post a comment
+    for commit in payload.get('commits', []):
+        commit_url = commit['url']
+        comment = "Your automated comment here"
+        comment_on_commit(commit_url, comment, github_api_token)
+
+def comment_on_commit(commit_url, comment, github_api_token):
+    headers = {'Authorization': f'token {github_api_token}'}
+    data = {'body': comment}
+    response = requests.post(commit_url, json=data, headers=headers)
+    return response
