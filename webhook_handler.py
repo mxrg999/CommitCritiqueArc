20ced65bb88673cb95502f1484a44cd5b292040d
mxrg999 <mxrg999@gmail.com>
2023-11-10 23:10:13 +0100

    changed the comment_on_commit

--- webhook_handler.py
+++ webhook_handler.py
@@ -7,8 +7,18 @@ def handle_push_event(payload, github_api_token):
         comment = "Your automated comment here"
         comment_on_commit(commit_url, comment, github_api_token)
 
+
+
+
 def comment_on_commit(commit_url, comment, github_api_token):
-    headers = {'Authorization': f'token {github_api_token}'}
+    # GitHub API URL to post a comment on a commit
+    comments_url = f"{commit_url}/comments"
+
+    headers = {
+        'Authorization': f'token {github_api_token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
     data = {'body': comment}
-    response = requests.post(commit_url, json=data, headers=headers)
+    
+    response = requests.post(comments_url, json=data, headers=headers)
     return response
