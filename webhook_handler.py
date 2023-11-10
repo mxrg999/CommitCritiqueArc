09fbb821f638bfaa4b433143c872dd725a08301a
mxrg999 <mxrg999@gmail.com>
2023-11-10 23:21:03 +0100

    changed the comments_url

--- webhook_handler.py
+++ webhook_handler.py
@@ -1,36 +1,26 @@
 import requests
 
 def handle_push_event(payload, github_api_token):
-    # Log the entire payload for debugging
-    print("Received payload:", payload)
-
+    repo_full_name = payload['repository']['full_name']
     for commit in payload.get('commits', []):
-        commit_url = commit['url']
+        commit_sha = commit['id']
         comment = "Your automated comment here"
-
-        print(f"Attempting to post comment on commit: {commit_url}")
-        response = comment_on_commit(commit_url, comment, github_api_token)
+        response = comment_on_commit(commit_sha, repo_full_name, comment, github_api_token)
 
         if response.status_code != 201:
-            print(f"Failed to post comment on {commit_url}. Status code: {response.status_code}")
+            print(f"Failed to post comment on {commit_sha}. Status code: {response.status_code}")
         else:
-            print(f"Successfully posted comment on {commit_url}. Response: {response.json()}")
+            print(f"Successfully posted comment on {commit_sha}. Response: {response.json()}")
 
-def comment_on_commit(commit_url, comment, github_api_token):
-    # Construct the GitHub API URL to post a comment on a commit
-    comments_url = f"{commit_url}/comments"
 
-    print(f"Posting to URL: {comments_url}")
+def comment_on_commit(commit_sha, repo_full_name, comment, github_api_token):
+    # Correct API URL for posting a comment
+    comments_url = f"https://api.github.com/repos/{repo_full_name}/commits/{commit_sha}/comments"
 
     headers = {
         'Authorization': f'token {github_api_token}',
         'Accept': 'application/vnd.github.v3+json'
     }
     data = {'body': comment}
-    
     response = requests.post(comments_url, json=data, headers=headers)
-
-    # Log the response for debugging
-    print(f"GitHub API Response: {response.status_code}, {response.text}")
-
     return response
