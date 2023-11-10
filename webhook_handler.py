8b31b508f585b556bd99eaf6f4672a0f46e29ad3
mxrg999 <mxrg999@gmail.com>
2023-11-10 23:28:45 +0100

    adding generate_comment

--- webhook_handler.py
+++ webhook_handler.py
@@ -4,13 +4,14 @@ def handle_push_event(payload, github_api_token):
     repo_full_name = payload['repository']['full_name']
     for commit in payload.get('commits', []):
         commit_sha = commit['id']
-        comment = "Your automated comment here"
+        comment = generate_comment(commit)
         response = comment_on_commit(commit_sha, repo_full_name, comment, github_api_token)
 
         if response.status_code != 201:
             print(f"Failed to post comment on {commit_sha}. Status code: {response.status_code}")
         else:
-            print(f"Successfully posted comment on {commit_sha}. Response: {response.json()}")
+            # print(f"Successfully posted comment on {commit_sha}. Response: {response.json()}")
+            print(f"Successfully posted comment on {commit_sha}")
 
 
 def comment_on_commit(commit_sha, repo_full_name, comment, github_api_token):
@@ -24,3 +25,19 @@ def comment_on_commit(commit_sha, repo_full_name, comment, github_api_token):
     data = {'body': comment}
     response = requests.post(comments_url, json=data, headers=headers)
     return response
+
+
+def generate_comment(commit):
+    # Get the commit message
+    commit_message = commit['message']
+
+    # Get the author name
+    author_name = commit['author']['name']
+
+    # Get the commit URL
+    commit_url = commit['url']
+
+    # Generate the comment
+    comment = f"Hey @{author_name}! I noticed that your commit message is '{commit_message}'. " \
+              f"Please remember to follow the commit message guidelines: " \
+              f"
