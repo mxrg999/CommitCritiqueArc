983d3bcbd375eec168d2bf55177005b9a7829daf
Max <mxrg999@gmail.com>
2023-12-11 21:31:32 +0100

    adding ollama to webhook_handler

--- webhook_handler.py
+++ webhook_handler.py
@@ -1,24 +1,29 @@
 import requests
-from openai import OpenAI
-from appcontext import AppContext
 from openai_connection import OpenAIConnection
-
-
+from ollama_connection import OllamaConnection
+import os
 
 def handle_push_event(payload, app_context, config_section):
-    openai_connection = OpenAIConnection(app_context.openai_api_key, config_section)
+    ai_service = os.getenv('AI_SERVICE', 'OpenAI').lower()
 
     repo_full_name = payload['repository']['full_name']
     for commit in payload.get('commits', []):
         commit_sha = commit['id']
-        comment = openai_connection.generate_comment(commit)
-        response = comment_on_commit(commit_sha, repo_full_name, comment, app_context.github_api_token)
 
-        if response.status_code != 201:
-            print(f"Failed to post comment on {commit_sha}. Status code: {response.status_code}")
+        if ai_service == 'ollama':
+            ollama_connection = OllamaConnection(config_section)
+            comment = ollama_connection.generate_comment(commit)
+            if comment is None:  # Fallback to OpenAI
+                comment = OpenAIConnection(app_context.openai_api_key, config_section).generate_comment(commit)
         else:
-            print(f"Successfully posted comment on {commit_sha}")
+            comment = OpenAIConnection(app_context.openai_api_key, config_section).generate_comment(commit)
 
+        if comment:
+            response = comment_on_commit(commit_sha, repo_full_name, comment, app_context.github_api_token)
+            if response.status_code != 201:
+                print(f"Failed to post comment on {commit_sha}. Status code: {response.status_code}")
+            else:
+                print(f"Successfully posted comment on {commit_sha}")
 
 
 def comment_on_commit(commit_sha, repo_full_name, comment, github_api_token):
